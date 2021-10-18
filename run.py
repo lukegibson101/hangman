96a785cc1119577bdac0604a88be7a383cf62893
Luke Gibson <lukegibson101@gmail.com>
2021-10-18 14:59:51 +0000

    set random word and main functions

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
 import random
 
+def get_random_word():
+    """
+    Picks a random word from words.txt to be used as the word the player has to guess. 
+    """
+    random_word = random.choice(open("words.txt").read().split('\n'))
+    return random_word.upper()
 
-# def get_random_word():
-random_line = random.choice(open("words.txt").read().split('\n'))
-print(random_line.upper())
+
+def main():
+    """
+    Runs the game 
+    """
+    hangman_word = get_random_word()
+    print("Lets play hangman!")
+
+
+main()
