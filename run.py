7a10164cd600c8300bfe53cf86a8e7b9dac0b0a1
Luke Gibson <lukegibson101@gmail.com>
2021-10-19 17:11:17 +0000

    add class for colored text

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
 import random
 
+class text_colors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    WHITE = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+ 
 def initialise_game():
     """
     Option to begin game or select difficulty
